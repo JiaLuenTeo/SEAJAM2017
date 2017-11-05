47afce2aaa1382eed0c9230f1453cfe4d4c5c970
Teo Jia Luen <jialuenteo@gmail.com>
2017-11-05 17:34:00 +0800

    v2.6 changed switchtab to support top view

--- Assets/Scripts/SwitchTabScript.cs
+++ Assets/Scripts/SwitchTabScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class SwitchTabScript : MonoBehaviour {
 
 	public RectTransform recipeGO,ingredientGO,upgradeGO;
+	public RectTransform recipeTOP,ingredientTOP,upgradeTOP;
 	bool isOut;
 
 	// Use this for initialization
@@ -26,8 +27,11 @@ public class SwitchTabScript : MonoBehaviour {
 		do
 		{
 			recipeGO.localPosition = Vector3.zero;
+			recipeTOP.localPosition = Vector3.zero;
 			ingredientGO.localPosition += Vector3.right;
+			ingredientTOP.localPosition += Vector3.right;
 			upgradeGO.localPosition += Vector3.right;
+			upgradeTOP.localPosition += Vector3.right;
 			if (ingredientGO.localPosition.x >= (ingredientGO.rect.max.x * 2) && upgradeGO.localPosition.x >= (upgradeGO.rect.max.x * 2))
 			{
 				isOut = true;
@@ -44,6 +48,9 @@ public class SwitchTabScript : MonoBehaviour {
 			recipeGO.localPosition += Vector3.right;
 			ingredientGO.localPosition = Vector3.zero;
 			upgradeGO.localPosition += Vector3.right;
+			recipeTOP.localPosition += Vector3.right;
+			ingredientTOP.localPosition = Vector3.zero;
+			upgradeTOP.localPosition += Vector3.right;
 			if (recipeGO.localPosition.x >= (recipeGO.rect.max.x * 2) && upgradeGO.localPosition.x >= (upgradeGO.rect.max.x * 2))
 			{
 				isOut = true;
@@ -59,6 +66,9 @@ public class SwitchTabScript : MonoBehaviour {
 			recipeGO.localPosition += Vector3.right;
 			ingredientGO.localPosition += Vector3.right;
 			upgradeGO.localPosition = Vector3.zero;
+			recipeTOP.localPosition += Vector3.right;
+			ingredientTOP.localPosition += Vector3.right;
+			upgradeTOP.localPosition = Vector3.zero;
 			if (recipeGO.localPosition.x >= (recipeGO.rect.max.x * 2) && ingredientGO.localPosition.x >= (ingredientGO.rect.max.x * 2))
 			{
 				isOut = true;
