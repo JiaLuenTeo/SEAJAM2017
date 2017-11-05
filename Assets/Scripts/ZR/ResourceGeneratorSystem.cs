3ebbc211efd39978c9111dd35885f652baa25213
Lai Ze Ren <laizr03@gmail.com>
2017-11-05 08:53:38 +0800

    v1.3 added stall upgrade and resource upgrade functions

--- Assets/Scripts/ZR/ResourceGeneratorSystem.cs
+++ Assets/Scripts/ZR/ResourceGeneratorSystem.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+[Serializable]
+public class MachineTier{
+
+	public int MaxCounter = 15;
+	public int MaxResourceReward = 4; // max number of resources gives out
+	public float TimeRequiredToCharge_Auto = 6f;
+	public List<string> ResourceNames; // resources that can be acquired in the tier
+	//TODO: ADD SPRITE IMAGE CHANGE
+}
+
+
 
 public class ResourceGeneratorSystem : MonoBehaviour {
 
-	public int MaxCounter = 15;
-	private int currCounter = 0; // no. of counters to spawn resource.
-	public int MaxResourceReward = 4; // max number of resources gives out
-	public float TimeRequiredToCharge_Auto = 1f;
-	public List<string> ResourceNames;
-	private float CurrTime = 0f;
+	public List<MachineTier> mMachineTier;
 
+
+	private float CurrTime = 0f;
+	private int currCounter = 0; // no. of counters to spawn resource.
 
 	//button func
 	public void HastenSpeed()
@@ -18,34 +29,67 @@ public class ResourceGeneratorSystem : MonoBehaviour {
 		currCounter -= 1;
 	}
 
+
+
+	//button func
+	public void UpgradeMachine(int cost)
+	{
+		if (PlayerPrefs.GetInt ("Machine_Tier") < 2) {
+			if (PlayerPrefs.GetInt ("Cash") >= cost) 
+			{
+				Debug.Log ("Upgrade purchased");
+				PlayerPrefs.SetInt ("Cash", PlayerPrefs.GetInt ("Cash") - cost);
+				PlayerPrefs.SetInt ("Machine_Tier", PlayerPrefs.GetInt ("Machine_Tier") + 1);
+				//TODO: Change machine visual
+			} 
+			else 
+			{
+				Debug.Log ("NO CASH!");
+			}
+		} 
+		else 
+		{
+			Debug.Log ("MAX TIER!");
+
+			//TODO: Add notification
+		}
+
+
+	}
+
+
 	void ProvideResources()
 	{
 		int randSeed=0;
 		int randAmount = 0;
 		if (currCounter <= 0)
 		{
-			randSeed = Random.Range (0, ResourceNames.Count);
-			randAmount = Random.Range (1, MaxResourceReward);
-			PlayerPrefs.SetInt (ResourceNames[randSeed] + "_CM", PlayerPrefs.GetInt (ResourceNames[randSeed]  + "_CM") + Random.Range(1,MaxResourceReward) + randAmount);
-			currCounter = MaxCounter;
+			randSeed = UnityEngine.Random.Range (0, mMachineTier[PlayerPrefs.GetInt ("Machine_Tier") ].ResourceNames.Count);
+			randAmount = UnityEngine.Random.Range (1, mMachineTier[PlayerPrefs.GetInt ("Machine_Tier") ].MaxResourceReward);
+
+			PlayerPrefs.SetInt (mMachineTier[PlayerPrefs.GetInt ("Machine_Tier") ].ResourceNames[randSeed] + "_CM", 
+				PlayerPrefs.GetInt (mMachineTier[PlayerPrefs.GetInt ("Machine_Tier") ].ResourceNames[randSeed]  + "_CM") 
+				+ UnityEngine.Random.Range(1,mMachineTier[PlayerPrefs.GetInt ("Machine_Tier") ].MaxResourceReward) + randAmount);
+			
+			currCounter = mMachineTier[PlayerPrefs.GetInt ("Machine_Tier") ].MaxCounter;
 		}
 	}
 
 	void OnEnable()
 	{
-		currCounter = MaxCounter;
+		currCounter = mMachineTier[PlayerPrefs.GetInt ("Machine_Tier") ].MaxCounter;
 	}
 	// Use this for initialization
 	void Start () 
 	{
-		currCounter = MaxCounter;
+		currCounter = mMachineTier[PlayerPrefs.GetInt ("Machine_Tier") ].MaxCounter;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		CurrTime += Time.deltaTime;
-		if (CurrTime >= TimeRequiredToCharge_Auto)
+		if (CurrTime >= mMachineTier[PlayerPrefs.GetInt ("Machine_Tier") ].TimeRequiredToCharge_Auto)
 		{
 			CurrTime = 0f;
 			ProvideResources ();
---
+++ Assets/Scripts/ZR/StallUpgradeManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+[Serializable]
+public class StallTier{
+
+	public List<GameObject> Recipes; // recipes that can be acquired in the tier
+
+}
+
+
+
+
+public class StallUpgradeManager : MonoBehaviour {
+
+	public List<StallTier> stallTier;
+	//button func
+	public void UpgradeStall(int cost)
+	{
+		if (PlayerPrefs.GetInt ("Stall_Tier") < 2) {
+			if (PlayerPrefs.GetInt ("Cash") >= cost) 
+			{
+				Debug.Log ("STALL Upgrade purchased");
+				PlayerPrefs.SetInt ("Cash", PlayerPrefs.GetInt ("Cash") - cost);
+				PlayerPrefs.SetInt ("Stall_Tier", PlayerPrefs.GetInt ("Stall_Tier") + 1);
+				TierCheck ();
+				//TODO: Change stall visual
+			} 
+			else 
+			{
+				Debug.Log ("NO CASH!");
+			}
+		} 
+		else 
+		{
+			Debug.Log ("MAX TIER!");
+
+			//TODO: Add notification
+		}
+
+
+	}
+
+	void TierCheck()
+	{
+		for (int i = 0; i < stallTier.Count; i++) 
+		{
+			if (stallTier [i] > PlayerPrefs.GetInt ("Stall_Tier")) {
+				foreach (GameObject trecipe in stallTier[i].Recipes) {
+					trecipe.SetActive (false);
+				}
+			}
+			else 
+			{
+				foreach (GameObject trecipe in stallTier[i].Recipes) {
+					trecipe.SetActive (true);
+				}
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		TierCheck ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
